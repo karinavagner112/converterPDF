b01e8363e62881f156c0576763f92ec7ec862e96
karinavagner112 <wagner_97@mail.ru>
2023-11-12 21:37:35 +0100

    main function structure

--- ConverterPDF/Program.cs
+++ ConverterPDF/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConverterPDF
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
 
