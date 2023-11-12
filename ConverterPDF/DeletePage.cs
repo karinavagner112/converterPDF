90ad9904d2c8fa4fb6a3abe04926cd6168dd3608
karinavagner112 <wagner_97@mail.ru>
2023-11-12 21:32:14 +0100

    added delete and merge function

---
+++ ConverterPDF/DeletePage.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace ConverterPDF
+{
+	public class DeletePage
+	{
+		public void DeletePageFromPDF(string sourcePdf, string targetPdf, List<int> pagesToRemove)
+		{
+			using (PdfDocument sourceDocument = PdfReader.Open(sourcePdf, PdfDocumentOpenMode.Import))
+			{
+				using(PdfDocument targetDocument = new PdfDocument())
+				{
+					for(int pageIndex = 0; pageIndex < sourceDocument.PageCount; pageIndex++)
+					{
+						if(!pagesToRemove.Contains(pageIndex + 1))
+						{
+							PdfPage page = sourceDocument.Pages[pageIndex];
+							targetDocument.AddPage(page); 
+						}
+					}
+
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                    targetDocument.Save(targetPdf);
+				}
+			}
+		}
+	}
+}
+
