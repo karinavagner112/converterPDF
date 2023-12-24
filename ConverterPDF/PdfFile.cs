c9bbc3598591b16c515aed84d453398e21679977
karinavagner112 <wagner_97@mail.ru>
2023-12-24 22:09:59 +0100

    added pdf file class

---
+++ ConverterPDF/PdfFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConverterPDF
+{
+    public class PdfFile
+    {
+        private string _basePath;
+
+        private string _newPath;
+
+        private string _pathSplit; 
+
+        public string Path
+        {
+            get { return _basePath; }
+            set { _basePath = value; }
+        }
+
+        public string NewPath
+        {
+            get { return _newPath; }
+            set { _newPath = value; }
+        }
+
+        public string PathSplit
+        {
+            get { return _pathSplit; }
+            set { _pathSplit = value; }
+        }
+    }
+}
+
--- ConverterPDF/Program.cs
+++ ConverterPDF/Program.cs
@@ -7,56 +7,57 @@ namespace ConverterPDF
         public static void Main(string[] args)
         {
             Console.WriteLine("Willkommen in PDF-Konverter");
-            Console.WriteLine("Welche Operation möchten Sie machen: " + 
+            Console.WriteLine("Welche Operation möchten Sie machen: " +
                 "1 - PDF zusammenfügen; 2 - Seiten löschen; 3 - PDF teilen");
+
             string option = Console.ReadLine();
-
             Helper helper = new Helper();
-            
+            PdfFile file = new PdfFile();
 
-            if(option == "1")
+
+            if (option == "1")
             {
                 Console.WriteLine("Geben Sie die Dateipfade von Pdfs, die zusammengefügt werden:");
-                string pdfPath = Console.ReadLine();
-                string[] paths = pdfPath.Split(' ');
+                file.Path = Console.ReadLine();
+                string[] paths = file.Path.Split(' ');
                 Console.WriteLine("Geben Sie den Dateipfad für die Zieldatei");
-                string newPath = Console.ReadLine();
+                file.NewPath = Console.ReadLine();
                 MergeFiles mergePdf = new MergeFiles();
-                mergePdf.MergePDFs(newPath, paths);
+                mergePdf.MergePDFs(file.NewPath, paths);
             }
-            else if(option == "2")
+            else if (option == "2")
             {
                 Console.WriteLine("Geben Sie die Dateipfade von Pdf-Datei, in der die Seiten gelöscht werden sollen: ");
-                string pdfPath = Console.ReadLine();
+                file.Path = Console.ReadLine();
                 Console.WriteLine("Geben Sie den Dateipfad für die Zieldatei");
-                string newPath = Console.ReadLine();
+                file.NewPath = Console.ReadLine();
                 Console.WriteLine("Welche Seiten sollen gelöscht werden? (Die Seitennummern durch Komma trennen.)");
                 string pages = Console.ReadLine();
                 string[] numberStrings = pages.Split(',');
                 List<int> numbersPdf = new List<int>();
                 helper.PagesToInt(numberStrings, numbersPdf);
                 DeletePage delete = new DeletePage();
-                delete.DeletePageFromPDF(pdfPath, newPath, numbersPdf);
+                delete.DeletePageFromPDF(file.Path, file.NewPath, numbersPdf);
 
             }
             else if (option == "3")
             {
                 Console.WriteLine("Geben Sie die Dateipfade von Pdf-Datei, die geteilt werden soll: ");
-                string pdfPath = Console.ReadLine();
+                file.Path = Console.ReadLine();
                 Console.WriteLine("Geben Sie den Dateipfade für die beiden Zieldatei");
-                string newPathOne = Console.ReadLine();
-                string newPathTwo = Console.ReadLine();
+                file.NewPath = Console.ReadLine();
+                file.PathSplit = Console.ReadLine();
                 Console.WriteLine("Auf welcher Seite sollte die PDF-Datei getrennt werden soll?");
                 string pageNumber = Console.ReadLine();
                 int number = int.Parse(pageNumber);
                 SplitPages split = new SplitPages();
-                split.SplitPdfPages(pdfPath, newPathOne, newPathTwo, number);
+                split.SplitPdfPages(file.Path, file.NewPath, file.PathSplit, number);
             }
             else
             {
-                Console.WriteLine("Falsche Eingabe! Versuchen Sie noch mal!");
+                Console.WriteLine("Eingabe ist falsch, versuchen Sie nochmal");
             }
-            
+
         }
     }
 }
